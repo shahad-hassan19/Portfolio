6c2094fdb64d7c6a23806eb3f566bfca63bec218
Shahad Hassan <shahadg1983@gmail.com>
2023-12-25 02:46:54 +0530

    skills sec updated

--- portfolio/src/Components/MySkills.js
+++ portfolio/src/Components/MySkills.js
@@ -12,7 +12,7 @@ export default function MySkills(){
                     <h2 className="text-center text-black mb-2"><span className="bg-yellow-300 px-2 ">My Skills</span></h2>
                     <h2 className="text-center font-semibold text-3.5xl mb-12 ">How can I help your project!</h2>
                 </div>
-                <div className="grid grid-cols-2">
+                <div className="grid sm:grid-cols-1 md:grid-cols-2">
                     <SkillCard icon={<FaHtml5/>} skill="HTML" content="HTML is the foundation of the web, allowing us to create user-friendly and captivating online experiences. With its building blocks, We craft websites that are both beautiful and easy to use. HTML isn't just code; it's the tool that brings our ideas to life."/>
                     <SkillCard icon={<FaCss3/>} skill="CSS" content="CSS is like the paintbrush for websites. It lets us create beautiful designs and make web pages look amazing. With CSS, We give websites their unique style and make sure they're easy to use and look great on any device."/>
                     <SkillCard icon={<FaJs/>} skill="JavaScript" content="JavaScript brings websites to life by making them interactive and fun to use. It helps developers create all sorts of cool things on the web, from flashy designs to making buttons do clever tricks."/>
